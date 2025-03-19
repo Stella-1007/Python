3183188468fa2ce3be908ccaba1c5fb72afc2607
Stella_11 <52149247+raglus@users.noreply.github.com>
2025-03-19 21:24:34 +0900

    Add files via upload

---
+++ inchTocm.py
@@ -0,0 +1,3 @@
+inch = float(input('cm로 변환할 값을 입력하십시오:'))
+cm = inch * 2.54
+print('변환된 값은', cm, 'cm 입니다')
---
+++ kgToPound.py
@@ -0,0 +1,3 @@
+kg = float(input('파운드로 변환할 값을 입력하십시오'))
+lbs = kg * 2.2
+print('변환한 파운드 값은', lbs,  'lbs 입니다.')
